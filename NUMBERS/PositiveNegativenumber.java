eeabc7d63fc828f76618aea57018fe7ca735fb7f
tejaskamble2003 <kambalet140@gmail.com>
2024-06-14 19:24:19 +0530

    PositiveNegativenumber

---
+++ NUMBERS/PositiveNegativenumber.java
@@ -0,0 +1,12 @@
+public class PositiveNegativenumber {
+    public static void main(String[] args) {
+        int n=0;
+        if(n<0){
+            System.out.println("Negative no");
+        }else if(n==0){
+            System.out.println("no is Zero");
+        }else{
+            System.out.println("Positive no");
+        }
+    }
+}
