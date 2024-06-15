50cbc438403a4a505c451cd30bd0a5b6ec5146ba
tejaskamble2003 <kambalet140@gmail.com>
2024-06-15 22:21:59 +0530

    Coding practice

---
+++ NUMBERS/EvenOddnumber.java
@@ -0,0 +1,10 @@
+public class EvenOddnumber{
+    public static void main(String args[]){
+        int n=201;
+        if(n%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+}
