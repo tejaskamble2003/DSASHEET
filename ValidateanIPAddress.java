e1976ab04f011101e660d2371fd1c6fb18ef1db6
tejaskamble2003 <kambalet140@gmail.com>
2024-08-06 21:13:09 +0530

    Validate An IP Address

---
+++ ValidateanIPAddress.java
@@ -0,0 +1,36 @@
+
+
+
+class ValidateanIPAddress {
+
+    public static boolean isValid(String str) {
+        // Write your code here
+        str = str + ".";
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c != '.') {
+                sb.append(c); 
+            }else if (sb.length() > 0) {
+                count++;
+                int val = Integer.parseInt(sb.toString());
+                if (val < 0 || val > 255) {
+                    return false;
+                }
+                if (sb.length() > 1 && sb.charAt(0) == '0') {
+                    return false;
+                }
+                sb.setLength(0);
+            } else {
+                return false;
+            }
+        }
+        return count == 4 ? true : false;
+    }
+
+    public static void main(String[] args) {
+        String str = "222.111.111.661";
+        System.out.println(isValid(str));
+    }
+}
